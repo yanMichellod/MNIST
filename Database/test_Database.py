dfa6c9360bd10f7120d3cccc69560ec150838a0c
Yan Michellod <yan.michellod@hotmail.com>
2021-09-20 13:55:51 +0200

    Remove unused test file and add test_database.py

--- Database/test_Database.py
+++ Database/test_Database.py
@@ -1,20 +1,10 @@
-import unittest
+from MNISTDatabase import loadMNISTDatabase
 import numpy as np
-import MNISTDatabase as db
-import math
 
-
-class Test(unittest.TestCase):
-
-    def test_load_data(self):
-        x_train, y_train, x_test, y_test = db.loadMNISTDatabase()
+def test_load_data():
+        x_train, y_train, x_test, y_test = loadMNISTDatabase()
         # Test shape of the arrays
         np.testing.assert_equal(x_train.shape, (60000, 28, 28))
         np.testing.assert_equal(len(y_train), 60000)
         np.testing.assert_equal(x_test.shape, (10000, 28, 28))
         np.testing.assert_equal(len(y_test), 10000)
-
-
-if __name__ == "__main__":
-
-    unittest.main()
