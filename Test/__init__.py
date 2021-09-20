877b4c1da5830f4bf7bdf0c4dc84ee80f88deecd
Yan Michellod <yan.michellod@hotmail.com>
2021-09-20 13:51:31 +0200

    update __init__ to add path of Preprocessing and Processor

--- Test/__init__.py
+++ Test/__init__.py
@@ -1,1 +1,3 @@
-from Database.MNISTDatabase import loadMNISTDatabase
+from Database.MNISTDatabase import loadMNISTDatabase,loadFromLocalFile
+from Preprocessing.Preprocessing import Preprocessing
+from Processor.Processor import get_baseline,define_model,runProcessor
