ade3bf161f6a70679634506e4f57701926f77c43
ralfjandl80 <65595079+ralfjandl80@users.noreply.github.com>
2021-10-01 16:43:22 +0200

    Solve error in analysis test

--- Test/test_Analysis.py
+++ Test/test_Analysis.py
@@ -6,6 +6,6 @@ def testAnalysis():
     Run several tests to check if Analysis works correct
     '''
     an = Analysis.Analysis(full=False)
-    assert self.an.checkVSBaseline()
-    assert self.an.checkHypothesis(0.7)
-    assert self.an.saveConfusionMatrix()
+    assert an.checkVSBaseline()
+    assert an.checkHypothesis(0.7)
+    assert an.saveConfusionMatrix()
