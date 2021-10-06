90e4ffc0f8b8eeaf0491090948c5ac077c15b0d7
ralfjandl80 <65595079+ralfjandl80@users.noreply.github.com>
2021-10-06 14:26:56 +0200

    Update main to run the 3 analysis

--- main/mnist.py
+++ main/mnist.py
@@ -2,4 +2,9 @@ from Analysis.Analysis import Analysis
 
 
 def main():
-    analysis = Analysis()
+    print("Start analysis of MNIST dataset...")
+    an = Analysis()
+    print("Is the CNN accuracy higher then the accuracy of the random forest: {0}".format(an.checkVSBaseline()))
+    print("Is the CNN accuracy higher then 0.95 (95%): {0}".format(an.checkHypothesis(0.95)))
+    print("Save confusion matrix: {0}".format(an.saveConfusionMatrix()))
+    print("Analysis done!")
