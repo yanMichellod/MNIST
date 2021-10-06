434d569c7d4f3cd491865b1424f5bbb98531a9c5
ralfjandl80 <65595079+ralfjandl80@users.noreply.github.com>
2021-10-06 16:51:07 +0200

    Remove option from CLI call

--- Test/test_main.py
+++ Test/test_main.py
@@ -3,7 +3,7 @@ import subprocess
 def test_main():
     """Run the cli command to test the whole analysis in test mode (full=False)
     """
-    proc = subprocess.Popen(["python3", "main/mnist.py",  "--full=False"],
+    proc = subprocess.Popen(["python3", "main/mnist.py"],
         stdout = subprocess.PIPE,
         stderr = subprocess.PIPE,
     )
